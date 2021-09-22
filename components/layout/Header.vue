cef93ce457cc247c79363d8bab3a34aeb49e2e5f
dimedrolll97@gmail.com <dimedrolll97@gmail.com>
2021-09-22 21:30:45 +0300

    Created header

--- components/layout/Header.vue
+++ components/layout/Header.vue
@@ -1,17 +1,26 @@
 <template>
-  <header class='header'>
-    <NuxtLink to='/'>Logo</NuxtLink>
-    <input type='text' class='search'>
-    <nav class='main-nav'>
-      <NuxtLink to='#'>Catalog</NuxtLink>
-      <NuxtLink to='#'>Community</NuxtLink>
-      <NuxtLink to='#'>Marketplace</NuxtLink>
-    </nav>
-  </header>
+  <div class='container'>
+    <header class='header'>
+      <NuxtLink class='logo' to='/'>
+        <img src='/icons/logo.svg' alt=''>
+      </NuxtLink>
+      <input type='text' class='search'>
+      <nav class='main-nav'>
+        <NuxtLink class='active' to='#'>Catalog</NuxtLink>
+        <NuxtLink to='#'>Community</NuxtLink>
+        <NuxtLink to='#'>Marketplace</NuxtLink>
+      </nav>
+      <user-bar/>
+    </header>
+  </div>
 </template>
 
 <script>
+import UserBar from '@/components/views/user/UserBar'
 export default {
+  components: {
+    'user-bar': UserBar
+  },
   name: 'Header'
 }
 </script>
---
+++ components/views/user/UserBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='user-bar'>
+    <div class='user-bar__items'>
+      <template v-if='userAuth'>
+        Auth user
+      </template>
+      <template v-else>
+        <NuxtLink class='login-link' to='/login'>Login</NuxtLink>
+      </template>
+      <button class='language'>
+        <img src='/icons/language.svg' alt=''>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserBar',
+  computed: {
+    userAuth() {
+      return false
+    }
+  }
+}
+</script>
