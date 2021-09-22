408984d999df794ad2f1435747fd23b2fc250edd
dimedrolll97@gmail.com <dimedrolll97@gmail.com>
2021-09-22 16:29:40 +0300

    Arch for murkup

---
+++ components/layout/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <header class='header'>
+    <NuxtLink to='/'>Logo</NuxtLink>
+    <input type='text' class='search'>
+    <nav class='main-nav'>
+      <NuxtLink to='#'>Catalog</NuxtLink>
+      <NuxtLink to='#'>Community</NuxtLink>
+      <NuxtLink to='#'>Marketplace</NuxtLink>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <header-component/>
+    <Nuxt />
+    <footer-component/>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/layout/Header';
+import Footer from '@/components/layout/Footer';
+
+  export default {
+    components: {
+      'header-component': Header,
+      'footer-component': Footer,
+    }
+  }
+</script>
