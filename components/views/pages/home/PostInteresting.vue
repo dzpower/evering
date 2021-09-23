785654c63415087c021704ece69592fd3062fb58
dimedrolll97@gmail.com <dimedrolll97@gmail.com>
2021-09-23 20:20:55 +0300

    Created post interesting, created slider

---
+++ components/views/pages/home/PostInteresting.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class='post-interesting'>
+      <figure>
+        <img src='/post-img.png' alt=''>
+        <figcaption>
+          How to get to the exhibition
+        </figcaption>
+      </figure>
+    <div class='post-interesting__body'>
+      <NuxtLink class='post-interesting__user' to='#'>
+          <img src='/user.png' alt=''>
+          <span>Alexandra Iks</span>
+      </NuxtLink>
+      <p>First of all, you need to decide on the date and city of the event...</p>
+      <NuxtLink class='post-interesting__comments' to='#'>
+        <img src='/icons/comments.svg' alt=''>
+        17 comments
+      </NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PostInteresting'
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
         <h2 class='default-h2'>
           Expert responses
         </h2>
-        <VueSlickCarousel :variable-width='true' :arrows="true">
+        <VueSlickCarousel :variable-width='true' :infinite='false' :arrows="true">
           <expert-response
             v-for='item in 15'
             :key='item'
@@ -13,6 +13,23 @@
             :title='item === 1 ? "Tips from puppet experts" : "New article"'
             :content='item === 1 ? "Hello, everyone. Today I want to tell you a little bit about my creative process, how to create images and collections, and share tips" : "Overview of the wizard"'
           />
+          <template #prevArrow>
+            <div class="custom-arrow">
+            </div>
+          </template>
+          <template #nextArrow>
+            <div class="custom-arrow custom-arrow__next">
+            </div>
+          </template>
+        </VueSlickCarousel>
+      </div>
+      <div class='most-interesting'>
+        <h2 class='default-h2'>The most interesting thing on Everigin</h2>
+        <VueSlickCarousel :variable-width='true' :arrows="true">
+            <post-interesting
+              v-for='i in 10'
+              :key='i'
+            />
           <template #prevArrow>
             <div class="custom-arrow">
             </div>
@@ -32,11 +49,13 @@ import VueSlickCarousel from 'vue-slick-carousel';
 import 'vue-slick-carousel/dist/vue-slick-carousel.css';
 import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
 import ExpertResponse from '@/components/views/pages/home/ExpertResponse';
+import PostInteresting from '@/components/views/pages/home/PostInteresting';
 
 export default {
     components: {
       VueSlickCarousel,
-      'expert-response': ExpertResponse
+      'expert-response': ExpertResponse,
+      'post-interesting': PostInteresting
     },
 }
 </script>
