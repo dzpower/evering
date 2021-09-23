f7929c504290c0b79f1d62941267cb376be7bb12
dimedrolll97@gmail.com <dimedrolll97@gmail.com>
2021-09-23 16:02:35 +0300

    Created experts responses block. Included slick slider

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,26 @@
 <template>
   <div class='home container'>
     <main class='main'>
-      <div class='experts-response'>
+      <div class='experts-responses'>
         <h2 class='default-h2'>
           Expert responses
         </h2>
-        <VueSlickCarousel :variableWidth='true' :arrows="true">
-          <div>1</div>
-
+        <VueSlickCarousel :variable-width='true' :arrows="true">
+          <expert-response
+            v-for='item in 15'
+            :key='item'
+            :class='item === 1 ? "first-item" : ""'
+            :title='item === 1 ? "Tips from puppet experts" : "New article"'
+            :content='item === 1 ? "Hello, everyone. Today I want to tell you a little bit about my creative process, how to create images and collections, and share tips" : "Overview of the wizard"'
+          />
+          <template #prevArrow>
+            <div class="custom-arrow">
+            </div>
+          </template>
+          <template #nextArrow>
+            <div class="custom-arrow custom-arrow__next">
+            </div>
+          </template>
         </VueSlickCarousel>
       </div>
     </main>
@@ -15,11 +28,15 @@
 </template>
 
 <script>
-import VueSlickCarousel from 'vue-slick-carousel'
-import 'vue-slick-carousel/dist/vue-slick-carousel.css'
-import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+import VueSlickCarousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css';
+import ExpertResponse from '@/components/views/pages/home/ExpertResponse';
 
 export default {
-    components: { VueSlickCarousel  },
+    components: {
+      VueSlickCarousel,
+      'expert-response': ExpertResponse
+    },
 }
 </script>
