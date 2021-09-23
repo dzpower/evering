7fe0e6cd082b62021309b6a1a4b8ab62e070095b
dimedrolll97@gmail.com <dimedrolll97@gmail.com>
2021-09-23 13:57:48 +0300

    Added slick slider

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,25 @@
 <template>
-  <h1>
-    Hello
-  </h1>
+  <div class='home container'>
+    <main class='main'>
+      <div class='experts-response'>
+        <h2 class='default-h2'>
+          Expert responses
+        </h2>
+        <VueSlickCarousel :variableWidth='true' :arrows="true">
+          <div>1</div>
+
+        </VueSlickCarousel>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
-export default {}
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
+
+export default {
+    components: { VueSlickCarousel  },
+}
 </script>
