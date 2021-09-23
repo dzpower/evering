17120329b6fd5897a1f4638404dc0f149ce9bad9
tsikoandrii <56772904+tsikoandrii@users.noreply.github.com>
2021-09-23 17:30:06 +0300

    create filter items

---
+++ components/views/pages/catalog/FilterItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="filter-item">
+    <span class="filter-item__title">Product type</span>
+
+    <a href="#" class="filter-item__link">
+      Dolls
+      <span>9 987 736</span>
+    </a>
+    <a href="#" class="filter-item__link">
+      Action figure
+      <span>5 965</span>
+    </a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "FilterItem"
+  }
+</script>
---
+++ pages/catalog.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="catalog container">
+    <main class="main">
+      <aside class="catalog-filter">
+
+        <FilterItem
+          v-for="item in 5"
+          :key="item"
+        ></FilterItem>
+
+      </aside>
+    </main>
+  </div>
+</template>
+
+<script>
+  import FilterItem from '@/components/views/pages/catalog/FilterItem';
+  export default {
+    components: {FilterItem}
+  }
+</script>
