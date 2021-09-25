bd22db025c8a41a85d6e8ec4e15d46b20af21528
dimedrolll97@gmail.com <dimedrolll97@gmail.com>
2021-09-25 18:48:12 +0300

    Finish home page, fixed bugs,crated any features

---
+++ components/views/pages/home/ProductPreview.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class='product-preview'>
+    <figure>
+      <img src='/product-preview.png' alt=''>
+    </figure>
+    <span class='title'>{{ title }}</span>
+    <div class='product-preview__body'>
+      <div class='product-preview__body-combiner'>
+        <span class='price'>{{ price }}</span>
+        <Rating :stars='rating'/>
+      </div>
+      <button class='buy'>
+        <img src='/icons/basket.svg' alt=''>
+      </button>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import Rating from '@/components/ui-common/Rating'
+
+export default {
+  name: 'ProductPreview',
+  components: {
+    Rating
+  },
+  props: {
+    title: {
+      type: String,
+      default: 'Product empty'
+    },
+    price: {
+      type: Number,
+      default: 0
+    },
+    rating: {
+      type: Number,
+      default: 1
+    }
+  }
+}
+</script>
--- pages/catalog.vue
+++ pages/catalog.vue
@@ -2,55 +2,40 @@
   <div class="catalog container">
     <main class="main">
       <aside class="catalog-filter">
-
         <FilterItem
           v-for="item in 5"
           :key="item"
         ></FilterItem>
-
       </aside>
-
       <div class="catalog-column">
         <div class="catalog-topbar">
           <div class="catalog-tags">
-
             <Tag
               v-for="(tag, index) in tags"
               :key="index"
               :href="tag.link"
             >
               {{tag.name}}</Tag>
-
           </div>
           <a href="#" class="catalog-mp-button">Search in the marketplace</a>
         </div>
         <div class="catalog-middlebar">
-
           <Pagination />
-
           <div class="catalog-sort">
             <span class="catalog-sort__label">Sorting</span>
-
             <Select />
-
           </div>
-
         </div>
         <div class="catalog-list">
-
           <Card
             v-for="item in 20"
             :key="item"
           ></Card>
-
         </div>
         <div class="catalog-bottombar">
-
           <Pagination />
-
         </div>
       </div>
-
     </main>
     <EmailSubscribe />
   </div>
@@ -62,10 +47,10 @@
   import Card from '@/components/ui-common/Card'
   import Tag from '@/components/ui-common/Tag';
   import Pagination from '@/components/ui-common/Pagination';
-  import Select from '@/components/ui-common/Select';
+  import Select from '@/components/ui-common/Select';;
 
   export default {
-    components: {Pagination, Tag, FilterItem, EmailSubscribe, Card, Select},
+    components: { Pagination, Tag, FilterItem, EmailSubscribe, Card, Select },
     data: () => {
       return {
         tags: [
