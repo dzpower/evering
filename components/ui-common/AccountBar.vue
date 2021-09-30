5fde31bb2cc489b8cf6029ef9201768bec1e99ea
tsikoandrii <56772904+tsikoandrii@users.noreply.github.com>
2021-09-30 18:52:29 +0300

    create purchases page

---
+++ components/ui-common/AccountBar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="account-bar">
+    <div class="account-bar__avatar">
+      <img src="/content/users/01.png" alt="account">
+    </div>
+    <div class="account-bar__wrapper">
+      <span class="account-bar__online">online</span>
+      <span class="account-bar__name">Svetlana</span>
+      <span class="account-bar__status reference__status reference__status--expert">Expert</span>
+      <div class="account-bar__stats">
+        <span>10000 Following</span>
+        <span>50000 Followers</span>
+      </div>
+      <EveButton icon class="account-bar__edit">
+        <template #icon>
+          <img src="/icons/edit.svg" alt="edit">
+        </template>
+        Edit a page
+      </EveButton>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import EveButton from '@/components/ui-common/EveButton';
+export default {
+  name: "Account",
+  components: {EveButton}
+}
+</script>
---
+++ pages/account/index.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="result-page catalog container">
+    <main class="main">
+      <aside class="result-page-filter">
+        <FilterItem
+          v-for="item in 5"
+          :key="item"
+        ></FilterItem>
+      </aside>
+      <div class="result-page-column">
+        <div class="result-page-topbar">
+          <div class="result-page-tags">
+            <Tag
+              v-for="(tag, index) in tags"
+              :key="index"
+              :href="tag.link"
+            >
+              {{tag.name}}</Tag>
+          </div>
+          <a href="#" class="result-page-mp-button">Search in the marketplace</a>
+        </div>
+        <div class="result-page-middlebar">
+          <Pagination />
+          <div class="result-page-sort">
+            <span class="result-page-sort__label">Sorting</span>
+            <Select />
+          </div>
+        </div>
+        <div class="result-page-list">
+          <Card
+            v-for="item in 20"
+            :key="item"
+            :to='`${$route.path}/${item}`'
+          ></Card>
+        </div>
+        <div class="result-page-bottombar">
+          <Pagination />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  import FilterItem from '@/components/views/pages/catalog/FilterItem'
+  import Card from '@/components/ui-common/Card'
+  import Tag from '@/components/ui-common/Tag'
+  import Pagination from '@/components/ui-common/Pagination'
+  import Select from '@/components/ui-common/Select'
+
+  export default {
+    components: { Pagination,
+      Tag,
+      FilterItem,
+      Card,
+      Select
+    },
+    data() {
+      return {
+        tags: [
+          {
+            name: 'Fashion',
+            link: '#'
+          },
+          {
+            name: 'Collectible',
+            link: '#'
+          },
+          {
+            name: 'Popular',
+            link: '#'
+          },
+          {
+            name: 'New products',
+            link: '#'
+          },
+          {
+            name: 'Recommendations',
+            link: '#'
+          },
+        ]
+      }
+    }
+  }
+</script>
