acdbdcd4da04a454aabd0032ef64225c8a13b476
tsikoandrii <56772904+tsikoandrii@users.noreply.github.com>
2021-10-04 17:30:20 +0300

    Create `communities` page

---
+++ components/views/pages/profile/Community.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="community">
+    <div class="community__avatar profile-bar__avatar">
+      <img src="/content/users/01.png" alt="image" />
+    </div>
+    <div class="community__wrapper">
+      <span class="community__name">Community Name</span>
+      <span class="community__subs">3459 participant</span>
+    </div>
+    <div class="community__btn">
+      <img src="/icons/3-dot.svg" alt="tool bar">
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: "Community"
+  }
+</script>
---
+++ pages/profile/communities.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="item-page profile-page container">
+    <main class="main">
+      <div class="item-page__columns">
+        <div class="item-page__leftcol">
+          <Navbar class="item-page-navbar" />
+        </div>
+        <div class="item-page__content">
+          <h2 class="default-h2 profile-page__title-btn">
+            <span class="profile-page__subtitle">Recently visited</span>
+            <EveButton>Clear</EveButton>
+          </h2>
+          <div class="profile-page__visited">
+
+            <!-- Community.vue -->
+            <div class="community community--visited">
+              <div class="community__avatar profile-bar__avatar">
+                <img src="/content/users/01.png" alt="image" />
+              </div>
+              <div class="community__wrapper">
+                <span class="community__name">Community Name</span>
+              </div>
+            </div>
+            <div class="community community--visited">
+              <div class="community__avatar profile-bar__avatar">
+                <img src="/content/users/01.png" alt="image" />
+              </div>
+              <div class="community__wrapper">
+                <span class="community__name">Community Name</span>
+              </div>
+            </div>
+
+          </div>
+          <RouterTabs :items="tabs" />
+          <div class="profile-page__pagination">
+            <Pagination/>
+          </div>
+          <div class="profile-page__communities">
+            <Community v-for="item in 5" :key="item" />
+          </div>
+          <div class="profile-page__pagination">
+            <Pagination/>
+          </div>
+        </div>
+        <div class="item-page__rightcol">
+          <SearchField class="profile-page__search" placeholder="Search" />
+          <h2 class="default-h2">Recommendations</h2>
+          <div class="profile-page__recommendations">
+            <Community v-for="item in 4" :key="item" class="community--small" />
+          </div>
+        </div>
+      </div>
+
+    </main>
+  </div>
+</template>
+
+<script>
+
+  import Navbar from '@/components/views/pages/profile/Navbar';
+  import SearchField from '@/components/ui-common/SearchField';
+  import RouterTabs from '@/components/ui-common/RouterTabs';
+  import EveButton from '@/components/ui-common/EveButton';
+  import Community from '@/components/views/pages/profile/Community';
+  import Pagination from '@/components/ui-common/Pagination';
+
+  export default {
+    components: {Pagination, Community, EveButton, RouterTabs, SearchField, Navbar},
+    data() {
+      return {
+        tabs: [
+          {
+            tabContent: 'Communities',
+            link: `/profile/communities`,
+            activeRouter: 'communities'
+          },
+          {
+            tabContent: 'Management',
+            link: `/profile/management`,
+            activeRouter: 'management'
+          }
+        ]
+      }
+    }
+  }
+</script>
