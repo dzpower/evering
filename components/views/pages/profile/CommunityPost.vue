4a5ddfd3e4910375406bd02ba24d8ec29b4804f7
tsikoandrii <56772904+tsikoandrii@users.noreply.github.com>
2021-10-05 16:31:32 +0300

    Create `community` page

---
+++ components/views/pages/profile/CommunityPost.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="community-post">
+    <Event />
+    <GalleryTiles />
+    <ActionBar />
+
+    <div class="community-post__reviews">
+      <Review v-for="i in 2" :key="i" />
+    </div>
+
+    <div class="community-post__pagination">
+      <Pagination />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import Event from '@/components/ui-common/Event';
+import GalleryTiles from '@/components/ui-common/photos/GalleryTiles';
+import ActionBar from '@/components/ui-common/ActionBar';
+import Review from '@/components/ui-common/Review';
+import Pagination from '@/components/ui-common/Pagination';
+
+export default {
+  name: "CommunityPost",
+  components: {Pagination, Review, ActionBar, GalleryTiles, Event}
+}
+</script>
---
+++ pages/profile/communities/_item.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="result-page profile-page container">
+    <main class="main">
+      <Navbar class="result-page-navbar" />
+      <div class="result-page-column">
+        <div class="profile-page__banner">
+          <img src="/content/banners/01.jpg" alt="banner">
+        </div>
+        <div class="profile-page__community-bar community-bar">
+          <h2 class="community-bar__title">Best of the best</h2>
+          <span class="community-bar__subs">10000 Following</span>
+          <EveButton class="community-bar__btn">Join</EveButton>
+        </div>
+        <div class="profile-page__columns">
+          <div class="profile-page__leftcol">
+
+            <CommunityPost
+              v-for="i in 3"
+              :key="i"
+            />
+
+          </div>
+          <div class="profile-page__rightcol">
+            <h2 class='default-h2'>Article</h2>
+            <div class="brand-page__articles">
+              <Article
+                v-for="item in 2"
+                :key="item"
+              />
+            </div>
+
+            <h2 class='default-h2'>References</h2>
+
+            <div class="brand-page__references">
+              <Reference
+                :expert="true"
+              />
+              <Reference
+                :expert="false"
+              />
+            </div>
+
+            <h2 class='default-h2'>Photo Albums</h2>
+            <div class="brand-page__articles">
+              <Article
+                v-for="item in 3"
+                :key="item"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+  import Navbar from '@/components/views/pages/profile/Navbar'
+  import EveButton from '@/components/ui-common/EveButton';
+  import CommunityPost from '@/components/views/pages/profile/CommunityPost';
+  import Article from '@/components/views/pages/brands/Article';
+  import Reference from '@/components/ui-common/Reference';
+
+  export default {
+    components: {Reference, CommunityPost, EveButton, Navbar, Article},
+  }
+</script>
