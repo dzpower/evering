9229a3425b014a4020069623da581203588225f9
tsikoandrii <56772904+tsikoandrii@users.noreply.github.com>
2021-10-06 14:47:10 +0300

    Create `cart` page

---
+++ components/views/pages/cart/CartItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="cart-item">
+    <div class="cart-item__image default-photo">
+      <img src="/content/01.png" alt="cart product image">
+    </div>
+    <div class="cart-item__wrapper">
+    <div class="cart-item__info">
+      <div class="cart-item__info-item">
+        <strong>Pullip optical Queen</strong>
+        <s>15800</s>
+        <mark>15800</mark>
+      </div>
+      <div class="cart-item__info-item">
+        Price
+        <span>$18.90</span>
+      </div>
+      <div class="cart-item__info-item">
+        Shipping
+        <span>$0.00</span>
+      </div>
+    </div>
+    <div class="cart-item__buttons">
+      <select class="default-select cart-item__select">
+        <option
+          v-for="i in 20"
+          :key="i"
+          class="default-select__option"
+          :value="i"
+        >
+          {{i}}
+        </option>
+      </select>
+      <button class="cart-item__delete">
+        <img src="/icons/delete.svg" alt="delete">
+      </button>
+    </div>
+  </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CartItem",
+}
+</script>
---
+++ pages/cart/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="cart-page container">
+    <main class="main">
+      <h2 class="default-h2">2 items in your cart</h2>
+      <div class="cart-page__columns">
+        <div class="cart-page__left-col">
+          <CartItem
+            v-for="item in 3"
+            :key="item"
+          />
+        </div>
+        <div class="cart-page__right-col">
+         <CartPrice />
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+
+import CartItem from '@/components/views/pages/cart/CartItem';
+import CartPrice from '@/components/views/pages/cart/CartPrice';
+export default {
+  components: {CartPrice, CartItem},
+}
+</script>
