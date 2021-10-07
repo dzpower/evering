4ec84ee96430613c5d37419c3d3c40712ec06526
tsikoandrii <56772904+tsikoandrii@users.noreply.github.com>
2021-10-08 00:27:06 +0300

    Create `video feed` page

---
+++ components/views/pages/feed/VideoItem.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="video-item">
+    <DefaultVideo/>
+    <div class="video-item__bottom-bar">
+      <div class="video-item__avatar">
+        <img src="/content/users/01.png" alt="">
+      </div>
+      <div class="video-item__wrapper">
+        <span class="video-item__title">
+          Master class on creating a doll
+        </span>
+        <br>
+        <span class="video-item__views">1780 views</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import DefaultVideo from '@/components/ui-common/DefaultVideo';
+export default {
+  name: "VideoItem",
+  components: {DefaultVideo}
+}
+</script>
